cc498e4166d9ef16039c9cc48bd582f2823ee859
Parkeymon <68636315+Parkeymon@users.noreply.github.com>
2021-05-27 15:06:28 -0700

    Add eventhandler

---
+++ AmongSCP/EventHandlers.cs
@@ -0,0 +1,17 @@
+﻿namespace AmongSCP
+{
+    public class EventHandlers
+    {
+        private readonly AmongSCP _plugin;
+
+        public EventHandlers(AmongSCP plugin)
+        {
+            _plugin = plugin;
+        }
+        
+        public void OnGameStart()
+        {
+            
+        }
+    }
+}
