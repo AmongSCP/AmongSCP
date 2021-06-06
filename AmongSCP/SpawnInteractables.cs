6ac2919336fb8187d3a9d674a46f7a0ce73f5423
smohan67 <61168189+smohan67@users.noreply.github.com>
2021-06-06 14:26:04 -0500

    Add Spawn Interactables

---
+++ AmongSCP/SpawnInteractables.cs
@@ -0,0 +1,28 @@
+﻿using AmongSCP.Map;
+using UnityEngine;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AmongSCP.Map.Interactables;
+using Exiled.API.Features;
+
+namespace AmongSCP
+{
+    public class SpawnInteractables
+    {
+        public SpawnInteractables()
+        {
+            Log.Debug("no");
+            SpawnEmergencyMeetingInteractable();
+        }
+
+        public void SpawnEmergencyMeetingInteractable()
+        {
+
+            ItemData emergencyInteractableData = new ItemData(ItemType.Flashlight, new Vector3(79.77f, -1005.87f, 40.07f), new Quaternion(0,0,0,0));
+            EmergencyMeetingInteractable emergencyInteractable = new EmergencyMeetingInteractable(emergencyInteractableData);
+        }
+    }
+}
