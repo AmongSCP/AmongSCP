9ee5a6b8a2f480af86f4d91376e1341f3ee13ec8
Parkeymon <68636315+Parkeymon@users.noreply.github.com>
2021-06-06 12:21:22 -0700

    meeting timer

--- AmongSCP/Util.cs
+++ AmongSCP/Util.cs
@@ -1,4 +1,6 @@
-﻿using Exiled.API.Features;
+﻿using System.Collections;
+using System.Collections.Generic;
+using Exiled.API.Features;
 using UnityEngine;
 using MEC;
 
@@ -6,19 +8,33 @@ namespace AmongSCP
 {
     public static class Util
     {
-        public static void CallEmergencyMeeting(Player caller, string message)
+        public static IEnumerator<float> CallEmergencyMeeting(Player caller, string message)
         {
+            Log.Debug("Emergency meeting invoked.");
+            var meetingStarted = true;
+            var meetingTime = AmongSCP.Singleton.Config.EmergencyTime;
+            
             foreach (var ply in Player.List)
             {
                 ply.Position = AmongSCP.Singleton.Config.VotePosition; 
             }
-            
-            Log.Debug("Emergency meeting invoked.");
+
+            while (meetingStarted)
+            {
+                Exiled.API.Features.Map.ShowHint($"Meeting ends in {meetingTime} seconds.", 1);
+                yield return Timing.WaitForSeconds(1f);
+                meetingTime -= 1;
+                if (meetingTime <= 1)
+                {
+                    meetingStarted = false;
+                }
+
+            }
         }
 
         public static void ReportBody()
         {
-
+            
         }
     }
 }
