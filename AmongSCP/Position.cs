bf2f1b3db4dade872e15fcbf7faace489e977d62
Parkeymon <68636315+Parkeymon@users.noreply.github.com>
2021-06-06 12:37:31 -0700

    add Position.cs for configs

---
+++ AmongSCP/Position.cs
@@ -0,0 +1,11 @@
+﻿namespace AmongSCP
+{
+    using UnityEngine;
+    
+    public class Position
+    {
+        public float X { get; set; } = new Vector3().x;
+        public float Y { get; set; } = new Vector3().y;
+        public float Z { get; set; } = new Vector3().z;
+    }
+}
