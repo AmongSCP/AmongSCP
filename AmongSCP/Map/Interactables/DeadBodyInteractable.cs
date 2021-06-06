582a22877a9d6ec20d2331fb06dce00975996d47
smohan67 <61168189+smohan67@users.noreply.github.com>
2021-06-06 18:15:12 -0500

    eeee

---
+++ AmongSCP/Map/Interactables/DeadBodyInteractable.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MEC;
+using UnityEngine;
+using Exiled.API.Features;
+
+namespace AmongSCP.Map.Interactables
+{
+    public class DeadBodyInteractable
+    {
+        private Interactable _interactable;
+
+        private Ragdoll _deadBody;
+
+        private String _name;
+
+        public DeadBodyInteractable(Ragdoll deadBody)
+        {
+            _deadBody = deadBody;
+            _name = deadBody.owner.Nick;
+
+            Vector3 deadBodyPosition = deadBody.transform.position;
+            deadBodyPosition.y += 1;
+
+            ItemData deadBodyItemData = new ItemData(ItemType.SCP018, deadBodyPosition, Quaternion.identity, new Vector3(2, 2, 2));
+
+            _interactable = new Interactable(deadBodyItemData, player =>
+            {
+                Timing.RunCoroutine(Util.CallEmergencyMeeting(player, player.Nickname + " has reported the body of" + _name));
+            }, true);
+        }
+    }
+}
