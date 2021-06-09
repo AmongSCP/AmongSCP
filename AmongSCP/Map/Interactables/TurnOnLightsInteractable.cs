7eb90999636d63550e4416052d4163105d0d1d19
smohan67 <61168189+smohan67@users.noreply.github.com>
2021-06-09 18:59:47 -0500

    add missing files

---
+++ AmongSCP/Map/Interactables/TurnOnLightsInteractable.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using Exiled.API.Features;
+
+namespace AmongSCP.Map.Interactables
+{
+    public class TurnOnLightsInteractable
+    {
+        Interactable _interactable;
+
+        public TurnOnLightsInteractable(Vector3 position, ItemType item)
+        {
+            Vector3 pos = position;
+            ItemData itemData = new ItemData(item, position, Quaternion.identity, new Vector3(2, 2, 2));
+            _interactable = new Interactable(itemData, player => Util.ModifyLightIntensity(1));
+        }
+    }
+}
