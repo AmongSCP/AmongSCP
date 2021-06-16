14d5e11adfb0b222d79e601c90ddc28a952e1d11
smohan67 <61168189+smohan67@users.noreply.github.com>
2021-06-15 21:38:52 -0500

    This works?Im confused

---
+++ AmongSCP/Patches/ImposterLightsPatch.cs
@@ -0,0 +1,30 @@
+﻿using Exiled.API.Extensions;
+using HarmonyLib;
+using UnityEngine;
+using Mirror;
+using Exiled.API.Features;
+
+namespace AmongSCP.Patches
+{
+    [HarmonyPatch(typeof(FlickerableLightController), nameof(FlickerableLightController.ServerSetLightIntensity))]
+    public static class ImposterLightsPatch
+    {
+        public static bool Prefix(FlickerableLightController __instance)
+        {
+            var networkIdentity = __instance.netIdentity;
+            foreach(Player ply in EventHandlers.PlayerManager.AlivePlayers)
+            {
+                if(EventHandlers.PlayerManager.Imposters.Contains(ply))
+                {
+                    MirrorExtensions.SendFakeTargetRpc(ply, networkIdentity, typeof(FlickerableLightController), nameof(FlickerableLightController.ServerSetLightIntensity), 1f);
+                }
+                else
+                {
+                    MirrorExtensions.SendFakeTargetRpc(ply, networkIdentity, typeof(FlickerableLightController), nameof(FlickerableLightController.ServerSetLightIntensity), 0f);
+                }
+            }
+            
+            return false;
+        }
+    }
+}
