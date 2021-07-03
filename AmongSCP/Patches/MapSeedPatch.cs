6fed7b83ba4c12d460c74c593f01809fc6fd03a0
smohan67 <61168189+smohan67@users.noreply.github.com>
2021-07-03 17:10:22 -0500

    Add

---
+++ AmongSCP/Patches/MapSeedPatch.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HarmonyLib;
+using MapGeneration;
+
+namespace AmongSCP.Patches
+{
+    [HarmonyPatch(typeof(SeedSynchronizer), nameof(SeedSynchronizer.Start))]
+    public static class MapSeedPatch
+    {
+        public static bool Prefix(SeedSynchronizer __instance)
+        {
+            __instance.Network_syncSeed = 1378514975;
+            return false;
+        }
+    }
+}
